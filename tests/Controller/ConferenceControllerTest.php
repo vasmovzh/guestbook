2e44604b7b2972294b693fa82e7f2ff83cf903bf
vasmovzh <vasmovzh@yandex.ru>
2021-10-24 18:07:56 +0700

    site scanning in functional conference controller test

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -15,4 +15,19 @@ final class ConferenceControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage(): void
+    {
+        $client  = self::createClient();
+        $crawler = $client->request('GET', '/');
+
+        self::assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        self::assertPageTitleContains('Tomsk');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorExists('div:contains("There are 4 comments")');
+        self::assertSelectorTextContains('h2', 'Tomsk 2020');
+    }
 }
